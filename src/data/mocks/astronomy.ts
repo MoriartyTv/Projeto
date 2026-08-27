5b1ce41d095024515f171807685bc2c853136809
Miguel <miguel.bittencourt.w@gmail.com>
2026-08-26 22:08:41 -0400

    Novo

--- src/data/mocks/astronomy.ts
+++
@@ -1,4 +0,0 @@
-export default [
-  { id:"a1", name:"Kepler-186f", type:"Exoplaneta", distance:"492 ly", magnitude:"+16.6", temperature:"-16°C", color:"#2bb3ff", description:"Planeta rochoso na zona habitável de sua estrela." },
-  { id:"a2", name:"TRAPPIST-1e", type:"Exoplaneta", distance:"40 ly", magnitude:"+18.9", temperature:"-30°C", color:"#66f0ff", description:"Membro do sistema TRAPPIST-1, candidato a estudo de atmosferas." }
-];
